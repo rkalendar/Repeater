2237d9711f442e44ae567418d0818718a13b0976
Ruslan Kalendar <78969155+rkalendar@users.noreply.github.com>
2024-08-01 23:44:54 +0500

    Add files via upload

--- src/SequencesClustering.java
+++ src/SequencesClustering.java
@@ -25,41 +25,39 @@ public final class SequencesClustering {
     }
 
     private int ClusteringPattern(String seq, int[] x1) {
-        int n = 0;
-        int kmer = 5;
         HashMap<String, Integer> pt = new HashMap<>();
         pt.put("aaatt", 0);
         pt.put("aagtt", 1);
-        pt.put("ataat", 3);
-        pt.put("atgat", 4);
-        pt.put("acagt", 5);
-        pt.put("acggt", 6);
-        pt.put("agact", 7);
-        pt.put("aggct", 8);
-        pt.put("ttaaa", 9);
-        pt.put("ttgaa", 10);
-        pt.put("taata", 11);
-        pt.put("tagta", 12);
-        pt.put("tgaca", 13);
-        pt.put("tggca", 14);
-        pt.put("tcaga", 15);
-        pt.put("tcgga", 16);
-        pt.put("ccagg", 17);
-        pt.put("ccggg", 18);
-        pt.put("caatg", 19);
-        pt.put("cagtg", 20);
-        pt.put("ctaag", 21);
-        pt.put("ctgag", 22);
-        pt.put("cgacg", 23);
-        pt.put("cggcg", 24);
-        pt.put("ggacc", 25);
-        pt.put("gggcc", 26);
-        pt.put("gaatc", 27);
-        pt.put("gagtc", 28);
-        pt.put("gtaac", 29);
-        pt.put("gtgac", 30);
-        pt.put("gcagc", 31);
-        pt.put("gcggc", 32);
+        pt.put("ataat", 2);
+        pt.put("atgat", 3);
+        pt.put("acagt", 4);
+        pt.put("acggt", 5);
+        pt.put("agact", 6);
+        pt.put("aggct", 7);
+        pt.put("ttaaa", 8);
+        pt.put("ttgaa", 9);
+        pt.put("taata", 10);
+        pt.put("tagta", 11);
+        pt.put("tgaca", 12);
+        pt.put("tggca", 13);
+        pt.put("tcaga", 14);
+        pt.put("tcgga", 15);
+        pt.put("ccagg", 16);
+        pt.put("ccggg", 17);
+        pt.put("caatg", 18);
+        pt.put("cagtg", 19);
+        pt.put("ctaag", 20);
+        pt.put("ctgag", 21);
+        pt.put("cgacg", 22);
+        pt.put("cggcg", 23);
+        pt.put("ggacc", 24);
+        pt.put("gggcc", 25);
+        pt.put("gaatc", 26);
+        pt.put("gagtc", 27);
+        pt.put("gtaac", 28);
+        pt.put("gtgac", 29);
+        pt.put("gcagc", 30);
+        pt.put("gcggc", 31);
 
         d = new int[nseq][2];
         for (int j = 0; j < nseq; j++) {
@@ -70,8 +68,10 @@ public final class SequencesClustering {
         Arrays.sort(d, (int[] a, int[] b) -> {
             return Integer.compare(b[1], a[1]);
         });
-
-        int[][] m2 = new int[nseq][33];
+        
+        int nkmers = pt.size();
+        int kmer = 5;
+        int[][] m2 = new int[nseq][nkmers];
         for (int j = 0; j < nseq; j++) {
             for (int i = 0; i < d[j][1] - kmer + 1; i++) {
                 String s = seq.substring(d[j][0] + i, d[j][0] + i + kmer);
@@ -85,8 +85,9 @@ public final class SequencesClustering {
             }
         }
 
-        // compare    
-        cx = new int[nseq];      // clusters           
+        // compare sequences  
+        int n = 0;              // amount of clusters
+        cx = new int[nseq];     // clusters           
         for (int i = 0; i < nseq; i++) {
             if (cx[i] == 0) {
                 n++;
@@ -94,9 +95,9 @@ public final class SequencesClustering {
 
                 for (int j = i + 1; j < nseq; j++) {
                     if (cx[j] == 0) {
-                        int[] m = new int[34];
+                        int[] m = new int[nkmers + 1];
 
-                        for (int k = 0; k < 33; k++) {
+                        for (int k = 0; k < nkmers; k++) {
                             if (m2[i][k] > 0 && m2[j][k] > 0) {
                                 if (m[0] == 0) {
                                     m[0] = 1;
@@ -108,12 +109,11 @@ public final class SequencesClustering {
                             }
                         }
 
-                        int v = 0;
-                        int z = 0;
+                        int v = 0;// pricatical matches 
+                        int z = 0;// theoretically maximum possible matches  
                         for (int k = 1; k < 1 + m[0]; k++) {
                             for (int y = k + 1; y < 1 + m[0]; y++) {
-                                double di = 0;
-                                double dj = 0;
+                                double di, dj;
                                 z++;
                                 if (m2[i][m[k]] < m2[i][m[y]]) {
                                     di = (double) m2[i][m[k]] / m2[i][m[y]];
@@ -135,7 +135,6 @@ public final class SequencesClustering {
                                         v++;
                                     }
                                 }
-
                             }
                             if (v > 0 && v + (v * dif) > z) {
                                 cx[j] = n;
